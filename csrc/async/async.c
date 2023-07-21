9517760fcad2afc11a183a3e9db32f78b9da148c
Gus Waldspurger <gus@waldspurger.com>
2023-07-21 11:55:22 -0700

    fix type

--- csrc/async/async.c
+++ csrc/async/async.c
@@ -40,7 +40,7 @@
 #include <time.h>
 
 #define LOG_STATE_CHANGE(label, entry) \
-    printf("%22s | %90s | %16p | %ld\n", label, entry->path, entry, getticks() % 10e7l)
+    printf("%22s | %90s | %16p | %ld\n", label, entry->path, entry, getticks() % 100000000)
 
 static __inline__ int64_t getticks(void)
 {
